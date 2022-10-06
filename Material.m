f781d0b015455f43aa7591f1608bf8911fd49c70
J.W. Kho <jwk33@cam.ac.uk>
2022-10-06 15:54:07 +0100

    Added Material class for tank structure and insulation

---
+++ Material.m
@@ -0,0 +1,13 @@
+classdef Material
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        name                    % material name
+        type                    % type (structural/insulator)
+        density                 % kg/m3
+        yield_strength          % MPa
+        thermal_conductivity    % W / m K
+    end
+end
+
