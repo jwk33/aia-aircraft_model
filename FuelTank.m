9f2037981393a7c1690002a3e2ffb6045cbadfd9
J.W. Kho <jwk33@cam.ac.uk>
2022-10-11 10:54:52 +0100

    Scaled internal tank dimensions to match max length and diameter

--- FuelTank.m
+++ FuelTank.m
@@ -3,15 +3,14 @@ classdef FuelTank < handle
     
     % set up fuel tank properties
     properties
-        intLength(1,1) double {mustBeNonnegative, mustBeFinite} % interior. diameter if sphere
-        intDiameter(1,1) double {mustBeNonnegative, mustBeFinite}
+        length_int(1,1) double {mustBeNonnegative, mustBeFinite} % interior. diameter if sphere
+        length_ext(1,1) double {mustBeNonnegative, mustBeFinite}
+        length_ext_max(1,1) double {mustBeNonnegative, mustBeFinite}
+        diam_ext_max(1,1) double {mustBeNonnegative, mustBeFinite}
+        diam_ext(1,1) double {mustBeNonnegative, mustBeFinite}
+        diam_int(1,1) double {mustBeNonnegative, mustBeFinite}
         fuelTankType (1,:) char {mustBeMember(fuelTankType,{'Cylinder', 'Sphere'})} = 'Cylinder';
-        structure_thickness(1,1) double {mustBeNonnegative, mustBeFinite}
-        insulation_thickness(1,1) double {mustBeNonnegative, mustBeFinite}
-        structure_mass(1,1) double {mustBeNonnegative, mustBeFinite}
-        insulation_mass(1,1) double {mustBeNonnegative, mustBeFinite}
-        empty_mass(1,1) double {mustBeNonnegative, mustBeFinite}
-        fuel_mass(1,1) double {mustBeNonnegative, mustBeFinite} % fuel mass (kg)
+        
         gravimetric_efficiency
     end
     
@@ -25,12 +24,14 @@ classdef FuelTank < handle
     end
     
     properties (SetAccess = private)
-        t_total double {mustBeNonnegative, mustBeFinite}
-        t_structure double {mustBeNonnegative, mustBeFinite}
-        t_insulation double {mustBeNonnegative, mustBeFinite}
-        m_empty double {mustBeNonnegative, mustBeFinite}
-        m_structure double {mustBeNonnegative, mustBeFinite}
-        m_insulation double {mustBeNonnegative, mustBeFinite}
+        fuel_mass(1,1) double {mustBeNonnegative, mustBeFinite} % fuel mass (kg)
+        fuel_volume(1,1) double {mustBeNonnegative, mustBeFinite} % fuel volume (m3)
+        t_total(1,1) double {mustBeNonnegative, mustBeFinite}
+        t_structure(1,1) double {mustBeNonnegative, mustBeFinite}
+        t_insulation(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_empty(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_structure(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_insulation(1,1) double {mustBeNonnegative, mustBeFinite}
     end
     
     properties (Constant)
@@ -49,7 +50,7 @@ classdef FuelTank < handle
                 fuel Fuel;
                 structural_material Material;
                 insulation_material Material;
-            end
+             end
             obj.fuel = fuel;
             obj.useTankModel = fuel.UseTankModel;
             obj.structural_material = structural_material;
@@ -57,9 +58,73 @@ classdef FuelTank < handle
             
         end
         
+        function obj = finalise(obj)
+            % uses all input tank properties to work out structural and
+            % insulation thickness + mass to get empty mass and gravimetric
+            % efficiency
+            
+            % find internal diameter/length given external diameter
+            obj.diam_int = obj.diam_ext_max;
+            obj.length_int = obj.length_ext_max;
+
+            obj.find_fuel_mass()
+            obj.find_t_total()
+
+            
+
+            while obj.diam_ext > obj.diam_ext_max
+                % update internal diameter so diam_ext matches diam_ext_max
+                obj.diam_int = obj.diam_int*0.999;
+
+                % find fuel volume and mass for given dimensions
+                obj.find_fuel_mass()
+                
+                % find thicknesses
+                obj.find_t_total()
+                
+                % update internal length to accomodate for the tank
+                % thickness
+                obj.length_int = obj.length_ext_max - 2*obj.t_total;
+            end
+
+            % find mass of insulation
+            obj.find_m_structure()
+            obj.find_m_insulation()
+
+            % put it all together: calculate tank empty mass and
+            % gravimetric efficiency
+            obj.find_t_total()
+            obj.find_m_empty()
+            obj.find_gravimetric_efficiency()
+        end
+
+
         function obj = find_gravimetric_efficiency(obj)
             % percentage of fuel weight out of total weight of tank 
-            obj.gravimetric_efficiency = 100 * obj.fuel_mass / (obj.fuel_mass + obj.empty_mass);
+            obj.gravimetric_efficiency = 100 * obj.fuel_mass / (obj.fuel_mass + obj.m_empty);
+        end
+        
+        
+        
+        function obj = volume(obj)
+            %calculate internal volume
+            formatSpec = char("Tank Internal Volume:\t %.2f (m^2) \n");
+            radius = obj.diam_int / 2;
+            caps_volume = 4*pi/3 * radius^3; % 2 hemispheres
+            cylinder_length = obj.length_int - obj.diam_int;
+            cylinder_volume = cylinder_length * pi * radius^2;
+            obj.fuel_volume = caps_volume + cylinder_volume;
+            
+            fprintf(formatSpec,obj.fuel_volume);
+        end
+       
+    end
+    methods (Access = private)
+
+        function obj = find_fuel_mass(obj)
+            % calculate fuel mass from tank volume
+            obj.volume()
+            obj.fuel_mass = obj.fuel.density * obj.fuel_volume;
         end
         
         function obj = find_t_total(obj)
@@ -68,6 +133,10 @@ classdef FuelTank < handle
             obj.find_t_insulation()
             
             obj.t_total = obj.t_structure + obj.t_insulation;
+
+            % update external dimensions
+            obj.length_ext = obj.length_int + 2*obj.t_total;
+            obj.diam_ext = obj.diam_int + 2*obj.t_total;
         end
         
         function obj = find_m_empty(obj)
@@ -77,71 +146,11 @@ classdef FuelTank < handle
            
            obj.m_empty = obj.m_structure + obj.m_insulation;
         end
-        
-        function obj = volume(obj)
-            %calculate internal volume
-            formatSpec = char("Tank Internal Volume:\t %.2f (m^2) \n");
-            radius = obj.diameter / 2;
-            caps_volume = 4*pi/3 * radius^3; % 2 hemispheres
-            cylinder_length = obj.length - obj.diameter;
-            cylinder_volume = cylinder_length * pi * radius^2;
-            tank_volume = caps_volume + cylinder_volume;
-            
-            fprintf(formatSpec,tank_volume);
-        end
-        
-        
-        function obj = updateInteriorLength(obj, wf, tow, dt)
-            arguments
-                obj
-                wf(1,1) double {mustBeInRange(wf,0,1)} % fuel fraction
-                tow(1,1) double {mustBePositive, mustBeFinite} % take-off weight
-                dt(1,1) double {mustBePositive, mustBeFinite} % tube diameter
-            end
-            % Update interior length of fuel tank, given aircraft param
-            obj.intLength = obj.interiorLength(wf, tow, dt);
-        end
-        function wfr = weightFrac(obj, dt, wf)
-            % obtain weight fraction of fuel tank
-            arguments
-                obj
-                dt(1,1) double {mustBePositive, mustBeFinite}% Tube diameter (external)
-                wf(1,1) double {mustBeInRange(wf,0,1)} % Fuel weight fraction
-            end
-            wfr = obj.fuelTankMass(dt, wf);
-            
-            if isnan(wfr)
-                wfr = 0;
-            end
-        end
-    end
-    methods (Access = private)
-        function df = interiorLength(obj, wf, tow, dt)
-            % Find interior length of fuel, given aircraft parameters
-            arguments
-                obj;
-                wf(1,1) double {mustBeInRange(wf,0,1)}; % Fuel weight/ MTOW
-                tow(1,1) double {mustBePositive, mustBeFinite}; % take-off weight
-                dt(1,1) double {mustBePositive, mustBeFinite}; % diameter of fuselage
-            end
-            rho = obj.fuel.density;
-            val1 = (wf*tow*1000)/(2*rho);
-            val2 = pi/6*((dt - 2*(obj.fusThickness + obj.excThickness)/1000 ...
-                )*obj.intExtFactor)^3;
-            if val1 > val2
-                df = (val1 - val2)*4/(pi*((dt - 2*(obj.fusThickness ...
-                    + obj.excThickness)/1000)*obj.intExtFactor)^2) ...
-                    + (dt - 2*(obj.fusThickness ...
-                    + obj.excThickness)/1000)*obj.intExtFactor;
-            else
-                df = (val1 * 6*pi)^(1/3);
-            end
-        end
-        
+
         function obj = find_t_structure(obj)
             % find structural thickness required for pressure specification
-            denominator = sqrt(1 - sqrt(3)*obj.design_pressure*0.1 / obj.structural_material.yield_stress);
-            thickness = obj.diameter/2 * (1/denominator - 1)*obj.safety_margin; % m
+            denominator = sqrt(1 - sqrt(3)*obj.design_pressure*0.1 / obj.structural_material.yield_strength);
+            thickness = obj.diam_int/2 * (1/denominator - 1)*obj.safety_margin; % m
             minimum_thickness = 7e-3;
             obj.t_structure = max(thickness, minimum_thickness);
         end
@@ -150,13 +159,13 @@ classdef FuelTank < handle
             
             % find structural mass given structural thickness
             
-            L = obj.length;
-            D = obj.diameter;
+            L = obj.length_int;
+            D = obj.diam_int;
             t = obj.t_structure;
             R = D/2;
             rho = obj.structural_material.density;
             cylinder_mass = rho*(pi*((t+R)^2 - R^2))*(L-D);
-            caps_mass = rho*4*pi/3 * ((t+R)^3 - R^3);
+            caps_mass = rho*4*pi/3 * ((t+R)^3 - R^3);   % mass of hemisphere
             obj.m_structure = cylinder_mass + caps_mass;
         end
         
@@ -178,8 +187,8 @@ classdef FuelTank < handle
             Q_boil = m_boil * H_vap;% W
 
             % heat transfer calculation to find insulation thickness
-            alpha = 2*pi*(Tins - Tliq)*obj.length/Q_boil;
-            R = obj.diameter / 2;
+            alpha = 2*pi*(Tins - Tliq)*obj.length_int/Q_boil;
+            R = obj.diam_int / 2;
             beta = (1/k_wall)*log((R + obj.t_structure)/R);
             gamma = alpha - beta;
             min_thickness = (R + obj.t_structure)*(exp(gamma*k_ins) - 1);
@@ -188,39 +197,14 @@ classdef FuelTank < handle
         end
         
         function obj = find_m_insulation(obj)
-            D = obj.diameter;
+            D = obj.diam_int;
             R = D / 2;   % m
-            L = obj.length;
-            t = obj.insulation_thickness;
-            w_t = obj.wall_thickness;
+            L = obj.length_int;
+            t = obj.t_insulation;
+            w_t = obj.t_structure;
             cylinder_mass = obj.insulation_material.density*pi*((R + w_t + t)^2 - (R + w_t)^2) * (L - D);
             caps_mass = obj.insulation_material.density*4*pi/3 * ((R + w_t + t)^3 - (R + w_t)^3);
             obj.m_insulation = cylinder_mass + caps_mass;
-        end
-        
-        
-        function fm = fuelTankMass(obj, dt, wf)
-            % Obtain estimate of fuelTankMass
-            arguments
-                obj
-                dt(1,1) double {mustBePositive, mustBeFinite} % Fuselage diameter
-                wf(1,1) double {mustBeInRange(wf,0,1)}% Fuel weight/ MTOW weight
-            end
-            if obj.useTankModel
-                if obj.intLength > dt
-                    % cylinder
-                    g = obj.gec(dt, obj.intLength);
-                    fm = (1/g - 1)*wf;
-                    obj.fuelTankType = 'Cylinder';
-                else
-                    % sphere
-                    g = obj.ges(obj.intLength);
-                    fm = (1/g - 1)*wf;
-                    obj.fuelTankType = 'Sphere';
-                end
-            else
-                fm = 0;
-            end
         end
         
     end
