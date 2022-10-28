c48e24ea45db3b90ea0654c3bc986f033c89e0ab
J.W. Kho <jwk33@cam.ac.uk>
2022-10-28 13:12:11 +0100

    Refactored weight class for weight model flexibility. Added kilian's torenbeek functions

--- Weight.m
+++ Weight.m
@@ -3,62 +3,134 @@ classdef Weight < handle
     %well as MTOW
 
     properties (SetAccess = public)
-        m_maxTO(1,1) double {mustBeNonnegative, mustBeFinite} % take-off weight in tons
+        % High level weights
+        m_maxTO(1,1) double {mustBeNonnegative, mustBeFinite} % take-off weight in kg
         m_OEW (1,1) double {mustBeNonnegative, mustBeFinite} %OEW = ZFW - max payload
-        m_shell(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_floor(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_max_payload(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_payload(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_fuel (1,1) double {mustBeNonnegative, mustBeFinite}
+
+        % Weight breakdown
+        m_wing (1,1) double {mustBeNonnegative, mustBeFinite}
+        m_fuselage (1,1) double {mustBeNonnegative, mustBeFinite}
+        m_LG(1,1) double {mustBeNonnegative, mustBeFinite} 
+        m_tail(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_engine (1,1) double {mustBeNonnegative, mustBeFinite}
+        m_fuel_sys(1,1) double {mustBeNonnegative, mustBeFinite}
         m_systems(1,1) double {mustBeNonnegative, mustBeFinite}
         m_furnishings(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_LG(1,1) double {mustBeNonnegative, mustBeFinite} 
+        m_op_items (1,1) double {mustBeNonnegative, mustBeFinite}
         m_seats(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_payload(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_max_payload(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_tail(1,1) double {mustBeNonnegative, mustBeFinite}
-        m_fuel_sys(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_shell(1,1) double {mustBeNonnegative, mustBeFinite}
+        m_floor(1,1) double {mustBeNonnegative, mustBeFinite}
     end
 
     methods
         function obj = Weight(aircraft)
             if aircraft.mission.range*0.02 - 19.79 > 30
-                obj.m_maxTO = aircraft.mission.range*0.02 - 19.79;
+                obj.m_maxTO = (aircraft.mission.range*0.02 - 19.79)*1e3;
             else
-                obj.m_maxTO = 30;
+                obj.m_maxTO = 30e3;
             end
-            obj.m_shell = 60 * aircraft.dimension.fuselage_diameter^2 * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_floor = 160 * 3.75^0.5 * aircraft.dimension.fuselage_diameter * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_systems = (270 * aircraft.dimension.fuselage_diameter + 150) * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_furnishings = (12 * aircraft.dimension.fuselage_diameter * (3 * aircraft.dimension.fuselage_diameter + aircraft.dimension.N_deck/2 + 1) * aircraft.dimension.cabin_length + 3500) /(1000*9.81);
-            obj.m_LG = 0.039 * (1 + aircraft.dimension.cabin_length / 1100) * obj.m_maxTO;
-            obj.m_seats = 350 * aircraft.mission.max_pax /(1000*9.81);
-            obj.m_payload = aircraft.mission.pax * 105/1000;   % Assume each passenger and their cargo weighs 105 kg
-            obj.m_max_payload = aircraft.mission.max_pax * 105/1000;
-            obj.m_tail = 0.07 * (obj.m_shell + obj.m_floor) + 0.1 * aircraft.aero.m_wing;
-            if ~isempty(aircraft.tank)
-                obj.m_fuel_sys = aircraft.tank.m_tank;
-            else
-                obj.m_fuel_sys = 0;
-            end
-            obj.m_OEW = aircraft.engine.m_eng + aircraft.aero.m_wing + obj.m_shell + obj.m_floor + obj.m_systems + obj.m_furnishings + obj.m_LG + obj.m_seats + obj.m_payload + obj.m_tail + obj.m_fuel_sys;
-            obj.m_maxTO = obj.m_OEW + obj.m_max_payload + aircraft.fuelburn.m_fuel;
+            
+            obj.m_payload = aircraft.mission.max_pax * aircraft.mission.load_factor * 105;  % Assume each passenger and their cargo weighs 105 kg
+            obj.m_max_payload = aircraft.mission.max_pax * 105;
+
+            obj = torenbeek(obj,aircraft);
+
+
+            % Operating Empty Weight
+            obj.m_OEW = obj.m_wing ...
+                + obj.m_fuselage ...
+                + obj.m_LG ...
+                + obj.m_tail ...
+                + obj.m_systems ...
+                + obj.m_furnishings ...
+                + obj.m_op_items ...
+                + obj.m_fuel_sys;
+            
+            % Max Takeoff Weight
+            obj.m_maxTO = obj.m_OEW + obj.m_max_payload + obj.m_fuel;
         end
 
         function obj = Weight_Iteration(obj,aircraft)
-            obj.m_shell = 60 * aircraft.dimension.fuselage_diameter^2 * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_floor = 160 * 3.75^0.5 * aircraft.dimension.fuselage_diameter * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_systems = (270 * aircraft.dimension.fuselage_diameter + 150) * aircraft.dimension.cabin_length /(1000*9.81);
-            obj.m_furnishings = (12 * aircraft.dimension.fuselage_diameter * (3 * aircraft.dimension.fuselage_diameter + aircraft.dimension.N_deck/2 + 1) * aircraft.dimension.cabin_length + 3500) /(1000*9.81);
+            obj.m_payload = aircraft.mission.max_pax * aircraft.mission.load_factor * 105;  % Assume each passenger and their cargo weighs 105 kg
+            obj.m_max_payload = aircraft.mission.max_pax * 105;
+
+            obj = torenbeek(obj,aircraft);
+
+
+            % Operating Empty Weight
+            obj.m_OEW = obj.m_wing ...
+                + obj.m_fuselage ...
+                + obj.m_LG ...
+                + obj.m_tail ...
+                + obj.m_systems ...
+                + obj.m_furnishings ...
+                + obj.m_op_items ...
+                + obj.m_fuel_sys;
+            
+            % Max Takeoff Weight
+            obj.m_maxTO = obj.m_OEW + obj.m_max_payload + obj.m_fuel;
+        end
+
+        function obj = torenbeek(obj,aircraft)
+            l = aircraft.dimension.fuselage_length;
+            d = aircraft.dimension.fuselage_diameter;
+            l_tank = 0;
+            g = 9.81;
+
+            % Wing mass
+            obj.m_wing = 0.86/sqrt(g) * (aircraft.aero.AR^2/aircraft.aero.wing_loading^3*obj.m_maxTO)^(1/4) * obj.m_maxTO;
+            
+            % Propulsion mass
+            obj.m_engine = aircraft.engine.m_engine;
+
+            % Landing Gear mass
+            obj.m_LG = 0.039*(1+l/1100) * obj.m_maxTO;
+
+            % Fuselage mass
+            obj.m_fuselage = (60 * d^2 * (l + 1.5) + 160 * sqrt(3.75) * d * l)/(g);
+
+            % Empennage mass
+            obj.m_tail = (0.1 * obj.m_wing) + (0.07 * obj.m_fuselage);
+
+            % Systems mass
+            obj.m_systems = (270*d + 150) * l/g;
+
+            % Furnishings mass
+            obj.m_furnishings = (35000 + (12*d*(3*d + aircraft.dimension.N_deck/2 + 1))*(l-l_tank))/g;
+
+            % Operating items mass
+            obj.m_op_items = 350 * aircraft.dimension.max_seats/g;
+
+            % Fuel Systems
+            if ~isempty(aircraft.tank)
+                obj.m_fuel_sys = aircraft.tank.m_tank;  %hydrogen tank
+            else
+                obj.m_fuel_sys = 0;
+            end
+
+            
+        end
+
+        function obj = torenbeek_old(obj,aircraft)
+            m_shell = 60 * aircraft.dimension.fuselage_diameter^2 * aircraft.dimension.cabin_length /(9.81);
+            m_floor = 160 * 3.75^0.5 * aircraft.dimension.fuselage_diameter * aircraft.dimension.cabin_length /(9.81);
+            
+            obj.m_fuselage = m_shell + m_floor;
+            
+            obj.m_systems = (270 * aircraft.dimension.fuselage_diameter + 150) * aircraft.dimension.cabin_length /(9.81);
+            obj.m_furnishings = (12 * aircraft.dimension.fuselage_diameter * (3 * aircraft.dimension.fuselage_diameter + aircraft.dimension.N_deck/2 + 1) * aircraft.dimension.cabin_length + 3500) /(9.81);
             obj.m_LG = 0.039 * (1 + aircraft.dimension.cabin_length / 1100) * obj.m_maxTO;
-            obj.m_seats = 350 * aircraft.mission.max_pax /(1000*9.81);
-            obj.m_payload = aircraft.mission.max_pax * aircraft.mission.load_factor * 105/1000;  % Assume each passenger and their cargo weighs 105 kg
-            obj.m_max_payload = aircraft.mission.max_pax * 105/1000;
-            obj.m_tail = 0.07 * (obj.m_shell + obj.m_floor) + 0.1 * aircraft.aero.m_wing/1000;
+            obj.m_op_items = 350 * aircraft.mission.max_pax /(9.81);
+            obj.m_tail = 0.07 * (obj.m_shell + obj.m_floor) + 0.1 * aircraft.aero.m_wing;
+
             if ~isempty(aircraft.tank)
                 obj.m_fuel_sys = aircraft.tank.m_tank;
             else
                 obj.m_fuel_sys = 0;
             end
-            obj.m_OEW = aircraft.engine.m_eng + aircraft.aero.m_wing + obj.m_shell + obj.m_floor + obj.m_systems + obj.m_furnishings + obj.m_LG + obj.m_seats + obj.m_payload + obj.m_tail + obj.m_fuel_sys;
-            obj.m_maxTO = obj.m_OEW + obj.m_max_payload + aircraft.fuelburn.m_fuel;
         end
     end
 end
