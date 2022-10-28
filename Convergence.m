a30a2766f979fed98bb2cdd913195b5224d8c0d1
J.W. Kho <jwk33@cam.ac.uk>
2022-10-28 10:07:13 +0100

    Added convergence class. Convergence settings and results are stored here.

---
+++ Convergence.m
@@ -0,0 +1,21 @@
+classdef Convergence
+    % This class will be used to store convergence settings and information
+    % about how well the solution converged.
+
+    properties
+        max_i (1,1) int8 {mustBePositive} = 50; % max iterations before code gives up
+        conv_margin (1,1) double {mustBePositive} = 0.0001; % percentage delta in mtow
+        MTOW (1,:) double % list of MTOW guesses on each iteration loop
+        conv_err (1,1) double = 9999; % error between current and previous iteration
+        conv_i (1,1) int8 % the number of iterations needed for solution to converge
+        conv_bool logical = 0; % true or false: true if solution has converged
+        
+    end
+
+    methods
+        function obj = Convergence()
+            % Create instance of convergence class. Defaults are set for
+            % convergence margin and max number of iterations.
+        end
+    end
+end
