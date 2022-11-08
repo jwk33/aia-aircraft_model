1ef947f27f83be3f6ba0151dfe8ca73d07e80fdb
J.W. Kho <jwk33@cam.ac.uk>
2022-11-08 10:56:53 +0000

    Added functionality to input tank dimensions into Dimension class

---
+++ Aircraft_Scaling_Test_H2.m
@@ -0,0 +1,56 @@
+close all
+clear all
+%% CONSTANT INPUTS
+
+load("LH2_Fuel.mat","LH2")
+
+range = 3790;%km
+M = 0.78;
+cruise_alt = 10000; %m
+max_pax = 180;
+cargo = 2959; %kg
+load_factor = 1.0;
+design_mission = Mission(range, M, cruise_alt, max_pax,load_factor, cargo);
+
+seats_per_row = 6;
+number_aisles = 1;
+N_deck = 1;
+
+dimension = Dimension(design_mission,seats_per_row,number_aisles,N_deck,1,0.5,0,0);
+dimension = dimension.finalise();
+
+%% Setup a fuel tank
+% define tank structural material
+density = 2700; %kg/m3
+yield_strength = 276e6; %Pa
+thermal_conductivity = 236; %W / m K
+struct_material = Material("Aluminium","Structural",density,yield_strength,thermal_conductivity);
+
+
+% define tank insulation material
+density = 50; %kg/m3
+yield_strength = 1e6; %Pa
+thermal_conductivity = 1e-4; %W / m K
+ins_material = Material("Insulation","Insulation",density,yield_strength,thermal_conductivity);
+
+
+h2_tank = FuelTank(LH2,struct_material, ins_material);
+h2_tank = h2_tank.finalise(dimension);
+
+%% SETUP AN INSTANCE OF AIRCRAFT CLASS
+
+H737 = Aircraft(LH2,design_mission,dimension);
+H737.tank = h2_tank;
+save('./saved-ac/H737.mat','H737');
+
+H737.m_eng_input = 4e3;
+H737.eta_input = 0.5;
+H737.AR_input = 10;
+H737.sweep_input = 30;
+H737.wing_area_input = 150;
+
+
+% All inputs defined. Now for the aircraft sizing loop to begin to
+% calculate MTOW
+H737 = H737.finalise();
+save('./saved-ac/H737.mat','H737');
--- Dimension.m
+++ Dimension.m
@@ -9,8 +9,10 @@ classdef Dimension
         cabin_length(1,1) double {mustBeNonnegative, mustBeFinite}
         cabin_height (1,1) double 
         aisle_length(1,1) double {mustBeNonnegative, mustBeFinite}
-        tank_external_diameter(1,1) double {mustBeNonnegative, mustBeFinite}
-        tank_external_length(1,1) double {mustBeNonnegative, mustBeFinite}
+        tank_external_diameter_u(1,1) double {mustBeNonnegative, mustBeFinite}
+        tank_external_length_u(1,1) double {mustBeNonnegative, mustBeFinite}
+        tank_external_diameter_i(1,1) double {mustBeNonnegative, mustBeFinite}
+        tank_external_length_i(1,1) double {mustBeNonnegative, mustBeFinite}
         N_deck(1,1) double {mustBeNonnegative, mustBeFinite}
         number_aisles double
         max_seats double
@@ -41,13 +43,25 @@ classdef Dimension
     end
 
     methods
-        function obj = Dimension(design_mission,seats_per_row,number_aisles,N_deck,tank_diameter,tank_length_percentage)
-            obj = Dimension_inline(obj,design_mission,seats_per_row,number_aisles,N_deck,tank_diameter,tank_length_percentage);
-        end
-        
-        function obj = Dimension_inline(obj,design_mission,seats_per_row,number_aisles,N_deck,tank_diameter,tank_length_percentage)
+        function obj = Dimension(design_mission,seats_per_row,number_aisles,N_deck,tank_diameter_i,tank_length_i,tank_diameter_u,tank_length_u)
             %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
+            %   The tank inputs are all as percentages. 
+            %   For under(over)floor tanks they are as a percentage of 
+            %   cabin width and cabin length
+            %   For inline it it as a percentage of cabin length and
+            %   fuselage diameter (after implementing underfloor)
+            
+            arguments
+                design_mission Mission
+                seats_per_row double
+                number_aisles double
+                N_deck double
+                tank_diameter_i double {mustBeGreaterThanOrEqual(tank_diameter_i,0)} % fraction of cabin width
+                tank_length_i double {mustBeGreaterThanOrEqual(tank_length_i,0)} % fraction of cabin length
+                tank_diameter_u double {mustBeGreaterThanOrEqual(tank_diameter_u,0)} % fraction of fuselage diameter
+                tank_length_u double {mustBeGreaterThanOrEqual(tank_length_u,0)} % fraction of cabin length
+            end
+
             obj.max_seats = design_mission.max_pax;
             obj.seats_per_row = seats_per_row;
             obj.number_aisles = number_aisles;
@@ -69,14 +83,24 @@ classdef Dimension
                 cabin_factor = 1.3;
             end
             obj.cabin_height = N_deck*(obj.seat_height + obj.bag_height) + obj.cargo_height;
-            obj.tank_external_diameter = tank_diameter;
+
+            obj.tank_external_diameter_u = tank_diameter_u*obj.cabin_width/100;
+
             min_fuselage_internal_diameter = (obj.cabin_width^2 + obj.cabin_height^2)^0.5;
-            d = obj.tank_external_diameter + 2*obj.tank_tolerance;
-            if d > min_fuselage_internal_diameter%id tank oversizes min fuselage diameter
-                obj.fuselage_internal_diameter = d;
+
+            d_u = obj.tank_external_diameter_u + 2*obj.tank_tolerance;
+            if d_u > sqrt(0.25*obj.cabin_width^2 + 0.25*obj.cabin_height) - obj.cabin_height/2
+                obj.fuselage_internal_diameter = (0.25*obj.cabin_width^2 + (d_u+obj.cabin_height)^2)/(d_u+obj.cabin_height);%if tank pushes fuselage to bottom
             else 
                 obj.fuselage_internal_diameter = min_fuselage_internal_diameter;%if tank is sufficiently small
             end
+        
+            obj.tank_external_diameter_i = tank_diameter_i*(obj.fuselage_internal_diameter - 2*obj.tank_tolerance);
+
+            d_i = obj.tank_external_diameter_i + 2*obj.tank_tolerance;
+            if d_i > obj.fuselage_internal_diameter%if tank oversizes min fuselage diameter
+                obj.fuselage_internal_diameter = d_i;
+            end
             obj.fuselage_diameter = obj.fuselage_internal_diameter + 2*obj.fuselage_thickness;
 
             if N_deck == 1
@@ -94,64 +118,23 @@ classdef Dimension
                 cab_length = ceil((overall_cab_length - 0.5*delta_cab_length*N_deck*(N_deck-1))/(obj.seat_length*N_deck))*obj.seat_length;% top cabin
                 obj.cabin_length = cab_length + (N_deck-1)*delta_cab_length;
             end
-            if tank_length_percentage > 100
-                obj.tank_external_length = obj.cabin_length;
-            elseif tank_length_percentage < 0
-                obj.tank_external_length = 0;
+            
+            obj.cabin_length = obj.cabin_length*cabin_factor;    
+
+            
+            obj.tank_external_length_u = tank_length_u*obj.cabin_length;
+            obj.tank_external_length_i = tank_length_i*obj.cabin_length;
+
+
+            if tank_length_u > 100
+                cab_length = obj.cabin_length*tank_lenght_u;
             else
-                obj.tank_external_length = tank_length_percentage*obj.cabin_length/100;
+                cab_length = obj.cabin_length;
             end
-            obj.cabin_length = obj.cabin_length*cabin_factor;
-            obj.fuselage_length = obj.cabin_length + obj.cockpit_length + obj.tank_external_length + obj.rear_length; 
+            obj.fuselage_length = cab_length + obj.cockpit_length + obj.tank_external_length_i + obj.rear_length;
         end
 
-        function obj = Dimension_overhead(obj,design_mission,seats_per_row,number_aisles,N_deck,tank_diameter,tank_length_percentage)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.max_seats = design_mission.max_pax;
-            obj.seats_per_row = seats_per_row;
-            obj.number_aisles = number_aisles;
-            obj.N_deck = N_deck; 
-            obj.cabin_width = obj.seat_width*obj.seats_per_row + obj.aisle_width*obj.number_aisles;
-            obj.cabin_height = N_deck*(obj.seat_height + obj.bag_height);
-            obj.cargo_height = 0.45*obj.cabin_height;
-            obj.cabin_height = obj.cabin_height + obj.cargo_height;
-            obj.tank_external_diameter = tank_diameter;
-            d = obj.tank_external_diameter + 2*obj.tank_tolerance;
-            if tank_diameter == 0
-                obj.fuselage_internal_diameter = (obj.cabin_width^2 + obj.cabin_height^2)^0.5;
-            elseif d > sqrt(0.25*obj.cabin_width^2 + 0.25*obj.cabin_height) - obj.cabin_height/2
-                obj.fuselage_internal_diameter = (0.25*obj.cabin_width^2 + (d+obj.cabin_height)^2)/(d+obj.cabin_height);%if tank pushes fuselage to bottom
-            else 
-                obj.fuselage_internal_diameter = (obj.cabin_width^2 + obj.cabin_height^2)^0.5;%if tank is sufficiently small
-            end
-            obj.fuselage_diameter = obj.fuselage_internal_diameter + 2*obj.fuselage_thickness;
-
-            if N_deck == 1
-                number_toilets = ceil(obj.max_seats/100);
-                obj.cockpit_length = obj.fuselage_diameter/(2*tand(obj.cockpit_angle));
-                obj.rear_length = obj.fuselage_diameter/(tand(obj.rear_angle));
-                obj.cabin_length = ceil(obj.max_seats/(obj.seats_per_row))*obj.seat_length + number_toilets*obj.toilet_length + obj.kitchen_length;
-                obj.fuselage_length = obj.cabin_length + obj.cockpit_length + obj.rear_length;
-            else
-                number_toilets = ceil(obj.max_seats/(N_deck*100));%avg toilets per deck % TODO: how many toilets per deck?
-                obj.cockpit_length = obj.fuselage_diameter/(2*tand(obj.cockpit_angle));
-                obj.rear_length = obj.fuselage_diameter/(tand(obj.rear_angle));
-%                 delta_cab_length = floor(obj.cabin_height/(obj.seat_length*tand(obj.rear_angle)))*obj.seat_length;%extra cabin space on each deck as you go up
-                delta_cab_length = 0;
-                overall_cab_length = ceil(obj.max_seats/(obj.seats_per_row))*obj.seat_length + number_toilets*N_deck*obj.toilet_length + N_deck*obj.kitchen_length;
-                cab_length = ceil((overall_cab_length - 0.5*delta_cab_length*N_deck*(N_deck-1))/(obj.seat_length*N_deck))*obj.seat_length;% top cabin
-                obj.cabin_length = cab_length + (N_deck-1)*delta_cab_length;
-                obj.fuselage_length = cab_length + obj.cockpit_length + obj.rear_length; 
-            end
-            if tank_length_percentage > 100
-                obj.tank_external_length = obj.cabin_length;
-            elseif tank_length_percentage < 0
-                obj.tank_external_length = 0;
-            else
-                obj.tank_external_length = tank_length_percentage*obj.cabin_length/100;
-            end
-        end
+        
 
         function obj = finalise(obj)
             %For now stick with the rectangle geometry. But have scope here
